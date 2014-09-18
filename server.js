0cf8edd0e20e07f09842cb5eac03ae5763f67d41
Emily To <emilytoe@gmail.com>
2014-09-18 01:37:09 -0700

    shortid fix

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-var express 				= require('express');
-var bodyParser 			= require('body-parser');
-var mongoose 				= require('mongoose');
-var shortid 				= require('shortid');
-var methodOverride 	= require('method-override');
-var app 						= express(); //create server
+var express             = require('express');
+var bodyParser          = require('body-parser');
+var mongoose            = require('mongoose');
+var methodOverride      = require('method-override');
+var app                 = express(); //create server
 
 // Port
 var port = 8888;
@@ -25,48 +24,53 @@ var router = express.Router();
 var Swatch = require('./app/models/swatch');
 
 router.use(function(req, res, next) {
-	console.log('Middleware');
-	next();
+    console.log('Middleware');
+    next();
 });
 
 router.route('/swatches')
-	.get(function(req, res) {
-		Swatch.find(function(err, swatches) {
-			if (err) {
-				res.send(err);
-			} else {
-				res.json(swatches);
-			}
-		});
-	})
-	.post(function(req, res) {
-    var swatch = new Swatch();
-    swatch.name = req.body.name;
-    swatch.blurb = req.body.blurb;
-    swatch.links = req.body.links;
-    swatch.color = req.body.color;
+    .get(function(req, res) {
+        Swatch.find(function(err, swatches) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(swatches);
+            }
+        });
+    })
+    .post(function(req, res) {
+        var swatch = new Swatch();
+        swatch.name = req.body.name;
+        swatch.blurb = req.body.blurb;
+        swatch.links = req.body.links;
+        swatch.color = req.body.color;
 
-    swatch.save(function(err) {
-      if (err) {
-        res.send(err);
-      } else {
-        res.json({ message: 'Swatch created!' });
-      }
+        swatch.save(function(err) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json({ message: 'Swatch created!' });
+            }
+        });
     });
-  });
 
-// router.route('/swatch/:swatchid')
-
-// 	.get(function(req,res) {
-
-// 	});
+router.route('/swatches/:swatchid')
+    .get(function(req,res) {
+        Swatch.findById(req.params.swatchid, function(err, swatch) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(swatch);
+            }
+        });
+    });
 
 app.use('/api', router);
 
 // Router
 // Frontend Route
 router.get('*', function(req, res) {
-	res.sendfile('./public/index.html');
+    res.sendfile('./public/index.html');
 });
 
 //start
