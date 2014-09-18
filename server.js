6d1e788c4c9e78fd50e73f8744cd537aa17bca75
Christian Le <christianle@berkeley.edu>
2014-09-18 01:11:02 -0700

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var shortid = require('shortid');
 var app = express(); //create server
+var methodOverride = require('body-parser');
 var Swatch = require('./app/models/swatch');
 
 //set our port
@@ -12,45 +13,45 @@ var port = 8888;
 mongoose.connect('mongodb://emto:pokemon@ds033740.mongolab.com:33740/testing');
 
 //create app with bodyparser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
 
 //create our router
 var router = express.Router();
 
-
 router.use(function(req, res, next) {
-	console.log('use function');
+	console.log('Middleware');
 	next();
 });
 
-
-router.get('*', function(req, res) {
-	res.sendfile('./public/views/index.html');
-});
-
-router.route('/swatch')
-	.post(function(req, res) {
-		var swatch = new Swatch();
-		swatch.name = req.body.name;
-		swatch.blurb = req.body.blurb;
-		swatch.color = req.body.color;
-		swatch.link = req.body.link;
-		swatch.save(function(err) {
-			if (err)
-				res.send(err);
-			res.json({ message : 'Swatch created!'});
-		});
-
-	})
-
+router.route('/swatches')
 	.get(function(req, res) {
-		Swatch.find(function(err, bears) {
-			if (err)
+		Swatch.find(function(err, swatches) {
+			if (err) {
 				res.send(err);
-			res.json(swatch);
+			} else {
+				res.json(swatches);
+			}
 		});
-	});
+	})
+	.post(function(req, res) {
+    var swatch = new Swatch();
+    swatch.name = req.body.name;
+    swatch.blurb = req.body.blurb;
+    swatch.links = req.body.links;
+    swatch.color = req.body.color;
+
+    swatch.save(function(err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json({ message: 'Swatch created!' });
+      }
+    });
+  });
 
 // router.route('/swatch/:swatchid')
 
@@ -60,6 +61,10 @@ router.route('/swatch')
 
 //register routes
 app.use('/api', router);
+
+router.get('*', function(req, res) {
+	res.sendfile('./public/index.html');
+});
 
 //start
 app.listen(port);
